726f31ba0eca22c0c42feda674d34f72c675bd5d
Guadalupe Vazquez <guvadlt@gmail.com>
2020-12-16 02:37:31 -0600

    cajas de fecha modificacion

--- app/src/main/java/com/example/ryoko/ModificarActivity.java
+++ app/src/main/java/com/example/ryoko/ModificarActivity.java
@@ -2,10 +2,13 @@ package com.example.ryoko;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -22,7 +25,7 @@ import org.json.JSONObject;
 import java.util.Calendar;
 import java.util.HashMap;
 
-public class ModificarActivity extends AppCompatActivity {
+public class ModificarActivity extends AppCompatActivity implements View.OnClickListener {
     EditText id_res,fecha_in,fecha_fn,nom,tp,trans,tt;
     Button btn_cambio;
 
@@ -56,7 +59,7 @@ public class ModificarActivity extends AppCompatActivity {
         nom.setText(reserva[3]+" "+reserva[4]+ " "+reserva[5]);
         tp.setText(reserva[6]);
         trans.setText(reserva[7]+", "+reserva[8]);
-        tt.setText(reserva[9]);
+        tt.setText("$ "+reserva[9]);
 
 
 
@@ -124,4 +127,41 @@ public class ModificarActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.fecha_in) {
+            obtenerFecha(fecha_in);
+        }
+        if (v.getId() == R.id.fecha_fn) {
+            obtenerFecha(fecha_fn);
+        }
+    }
+
+
+    private void obtenerFecha(EditText fecha){
+        DatePickerDialog recogerFecha = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                //Esta variable lo que realiza es aumentar en uno el mes ya que comienza desde 0 = enero
+                final int mesActual = month + 1;
+                //Formateo el día obtenido: antepone el 0 si son menores de 10
+                String diaFormateado = (dayOfMonth < 10)? CERO + String.valueOf(dayOfMonth):String.valueOf(dayOfMonth);
+                //Formateo el mes obtenido: antepone el 0 si son menores de 10
+                String mesFormateado = (mesActual < 10)? CERO + String.valueOf(mesActual):String.valueOf(mesActual);
+                //Muestro la fecha con el formato deseado
+                fecha.setText(diaFormateado + BARRA + mesFormateado + BARRA + year);
+
+
+            }
+            //Estos valores deben ir en ese orden, de lo contrario no mostrara la fecha actual
+            /**
+             *También puede cargar los valores que usted desee
+             */
+        },anio, mes, dia);
+        //Muestro el widget
+        recogerFecha.show();
+    }
+
+
+    
 }
